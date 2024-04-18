f6299443ab615b282a41137812b9f8bec7a3a4e5
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-04-18 12:12:03 -0700

    initial commit - dependencies added, initial file structure created

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require ('express');
+const path = require('path');
+const api = require('./routes');
+// const axios = require('axios');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+if (process.end.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../client/dist/index.html')));
+}
+
+// app.get('*', (req, res) => 
+//   res.sendFile(path.join(__dirname, '/public/'))
+// )
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+})
+
+
