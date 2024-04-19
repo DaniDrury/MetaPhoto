0934062bb119963834ca151e94a76f84b6f6de17
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-04-18 21:34:49 -0700

    adding filtering - title and albumTitle complete but not optimized - pagination undone by filtering requirements

--- server/routes/photos.js
+++ server/routes/photos.js
@@ -7,22 +7,56 @@ const photosApi = "https://jsonplaceholder.typicode.com/photos";
 
 router.get('/', async (req, res) => {
   const query = req.query;
-
+  // console.log(query);
   const limit = query.limit || 25;
   const offset = query.offset || 0;
+  const title = query.title;
+  const albumTitle = query['album.title'];
+  const userEmail = query['album.user.email'];
+
+  // console.log("Title is " + title);
 
   try {
-    const photoResponse = await axios.get(`${photosApi}?_start=${offset}&_limit=${limit}`);
-    const photoData = photoResponse.data;
+    // get all photos
+    const allPhotosResponse = await axios.get(`${photosApi}`);
+    const allPhotos = allPhotosResponse.data;
 
+    let photoData = [];
+
+    if (title) {
+      // separate out only those with title containing query title
+      photoData = allPhotos.filter((photo) => photo.title.includes(title));
+    }
+
+    if (albumTitle) {
+      const allAlbumsResponse = await axios.get(`${albumsApi}`);
+      const allAlbums = allAlbumsResponse.data;
+
+      const albumMatches = allAlbums.filter((album) => album.title.includes(albumTitle));
+
+      // console.log(albumMatches);
+
+      // builds a new array of photos with albumId equal to albumMatches.id -- O(n^2) time complexity :(  Not ideal
+      for (let i=0; i < allPhotos.length; i++) {
+        for (let j = 0; j < albumMatches.length; j++) {
+          if (allPhotos[i].albumId === albumMatches[j].id) {
+            photoData.push(allPhotos[i]);
+          }
+        }
+      }
+    }
+    
     const photoDataEnriched = [];
 
+    // for each photo that meets the filter specifications, embed album and user info
     for (let i = 0; i < photoData.length; i++) {
       const albumResponse = await axios.get(`${albumsApi}/${photoData[i].albumId}`);
       const albumData = albumResponse.data;
+      // console.log("got album from jsonplaceholder");
   
       const userResponse = await axios.get(`${userApi}/${albumData.userId}`);
       const userData = userResponse.data;
+      // console.log("got user from jsonplaceholder");
 
       // removed userId key from albumData
       const { userId, ...album } = albumData
@@ -35,6 +69,7 @@ router.get('/', async (req, res) => {
 
       photoDataEnriched.push(photo);
     }
+    console.log("Return Length: " + photoDataEnriched.length);
 
     res.json(photoDataEnriched);
   } catch (err) {
